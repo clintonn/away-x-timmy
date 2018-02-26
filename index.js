8f4dafc7b585768826dab5f657d8354027d2e5ff
clintonn <clinton.ngyn@gmail.com>
2018-02-25 22:52:24 -0500

    Add some mixins and a weight of Reckless

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import './style';
 import { Component } from 'preact';
+import './styles/index.scss';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>
+			<div className='Main'>
 				<h1>Hello, World!</h1>
+				<p>Lectus tincidunt nam urna etiam eros commodo dictum, lacus sociosqu duis rutrum porttitor pellentesque, lobortis ridiculus accumsan felis conubia sodales. Tortor scelerisque aliquet lectus etiam platea, varius dignissim mi gravida taciti massa, hac nisl integer maecenas.</p>
 			</div>
 		);
 	}
